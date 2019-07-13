fddcb3f1f5579bbbd157938113bd18b957dcfa2e
Andrew Parlane <andrew.parlane@gmail.com>
2019-07-12 23:06:13 -0300

    Increased clock period to 11ns.
    Alos automated the calculation of the input and output delays.
    Now you just have to specify what percentage of the available time goes where.

--- micro/backend/scripts/syn/constraints.tcl
+++ micro/backend/scripts/syn/constraints.tcl
@@ -88,11 +88,49 @@
 # Edit synthesis contraints here!!!
 #####################################################################################
 
-create_clock -name _iClk -period 10 [get_ports _iClk]
+set period 11.0
+create_clock -name _iClk -period ${period} [get_ports _iClk]
 
 set_false_path -from [get_ports _iReset]
 
-set_input_delay -clock [get_clock _iClk] -max 8.9 [get_ports _iInstMemData]
-set_input_delay -clock [get_clock _iClk] -max 8.9 [get_ports _iDataMemRData]
+# data changes 8.9ns after addr changes.
+# writes finish 8.9ns after Write asserted.
+set ramDelay 8.9
 
+# This means there is $period - $ramDelay free.
+# In that time we have to:
+#  1) get the output signals from the FF to the port of our core.
+#  2) get the output signals from the port of our core to the RAM.
+#  3) get the input data from the RAM to the port of our core.
+#  4) get the input data from the port of our core to the FF (including setup)
+# I'm assuming that 2) and 3) are 0ns in my case. I probably should account
+# for these, but without knowing if the SRAM is off / on chip, it's hard to
+# guess.
+# I'm therefore splitting the available time between 1) and 4).
+# 1) gets outputSharePercent
+set extra [expr {${period} - ${ramDelay}}]
+set outputSharePercent 0.7
+set outputShare [expr {${extra} * ${outputSharePercent}}]
+
+# First we must ensure that the outputs get out of our core in a timely
+# fashion. If we specify that there's an output delay of ${period} - 
+# ${outputShare} then the tools have to find a path that takes only
+# ${outputShare} or less to go from the register to the port.
+set outputDelay [expr {${period} - ${outputShare}}]
+
+set_output_delay -clock [get_clock _iClk] ${outputDelay} [get_ports _oInstMemAddr]
+set_output_delay -clock [get_clock _iClk] ${outputDelay} [get_ports _oDataMemAddr]
+set_output_delay -clock [get_clock _iClk] ${outputDelay} [get_ports _oDataMemWData]
+set_output_delay -clock [get_clock _iClk] ${outputDelay} [get_ports _oDataMemWrite]
+
+# Next We then set an input delay on the Data inputs.
+# This is the delay from the Addr register to the RAM
+# and the $ramDelay it takes the data to go valid.
+# This means our core has to get the input data from the port
+# to the input FF in $period - $inputDelay
+# (including the setup time of the FF).
+set inputDelay [expr {${outputShare} + ${ramDelay}}]
+
+set_input_delay -clock [get_clock _iClk] ${inputDelay} [get_ports _iInstMemData]
+set_input_delay -clock [get_clock _iClk] ${inputDelay} [get_ports _iDataMemRData]
 
